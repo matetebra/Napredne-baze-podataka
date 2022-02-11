100874f66aee073fece5dd723f99dbf50c11a41d
natalijapavlovic17321 <nnatanna@elfak.rs>
2022-02-11 18:07:55 +0100

    g

--- Aplikacija/Profil/assets/js/restorani.js
+++ Aplikacija/Profil/assets/js/restorani.js
@@ -1,8 +1,33 @@
+import { restoran } from "./restoran.js";
+import { korisnik } from "./korisnik.js";
 
 export class restorani{
     constructor(){
-        
+      this.restorani=[];
     }
+    
+ucitajRestorane(){
+  fetch("https://localhost:7284/Slavko/GetRestourants").then((p) => {
+      p.json().then((data) => {
+        data.forEach((element) => {
+          var r = new restoran();
+          r.naziv=element.naziv;
+          r.adresa=element.adresa;
+          r.email=element.email;
+          r.telefon=element.telefon;
+          this.restorani.push(r);
+        });
+        this.crtajsveRestorane();
+      });
+    });
+  }
+
+crtajsveRestorane(){
+  const host=document.getElementById("restorani");
+  this.restorani.forEach((rest)=>{
+      rest.crtajRestoran(host)
+  });
+}
     preuzmiNeodobrene(){
         fetch("https://localhost:7284/Slavko/GetRestourantsNeodobreni", {
             method: "GET",
