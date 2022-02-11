5e2a488d42bf7c6be51718cc436ddab36d0dfe53
unknown <djokicaleksandra99@gmail.com>
2022-02-11 17:31:42 +0100

    user front

---
+++ Aplikacija/Profil/assets/js/korisnik.js
@@ -0,0 +1,34 @@
+
+export class korisnik{
+    constructor(email, ime, prezime, adresa, grad, telefon){
+        this.email=email;
+        this.adresa=adresa;
+        this.ime=ime;
+        this.prezime=prezime;
+        this.grad=grad;
+        this.telefon=telefon;
+        this.omiljeniRestorani=[];
+        this.porudzbina=[];
+        this.trenutnaPorudzbina=[];
+
+    }
+    crtajInformacije(){
+        var inf=document.getElementById("informacije");
+        var pom=document.getElementById("div");
+        
+        var ime=document.createElement("h1");
+        ime.innerHTML=this.ime+" "+this.prezime;
+        ime.classList.add("text-light");
+        inf.appendChild(ime);
+        var mejl=document.createElement("h1");
+        mejl.innerHTML=this.email;
+        mejl.classList.add("text-light");
+        inf.appendChild(mejl);
+        var br=document.createElement("h1");
+        br.innerHTML=this.telefon;
+        br.classList.add("text-light");
+        inf.appendChild(br);
+
+    }
+
+}
