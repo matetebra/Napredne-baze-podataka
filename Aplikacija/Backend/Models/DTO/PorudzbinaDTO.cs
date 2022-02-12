d3e42998488920c80a3672065aca5b5c9cd49168
Slavko Petrovic <93668295+SlavkoPetrovic@users.noreply.github.com>
2022-02-12 01:10:47 +0100

    k

--- Aplikacija/Backend/Models/DTO/PorudzbinaDTO.cs
+++ Aplikacija/Backend/Models/DTO/PorudzbinaDTO.cs
@@ -4,10 +4,7 @@ using MongoDB.Driver;
 public class PorudzbinaDTO
 {
     public String? Napomena { get; set; }
-    public Boolean Dostavljena { get; set; }
-    public DateTime Datum { get; set; }
     public List<String>? JelaID { get; set; }
     public String? EmailRestoran { get; set; }
     public List<String>? DodaciID { get; set; }
-    public int UkupnaCena { get; set; }
 }
